6796889eb9921dc2443789135af8943eec993732
Aleksandr Volkov <guthub.volkov@gmail.com>
2022-10-17 19:55:42 +0300

    Изменил мем-функцию

--- src/memoize.js
+++ src/memoize.js
@@ -1,3 +1,21 @@
-module.exports = ()=>{
-    return 1;
+module.exports = (fn)=>{
+    const cache = {}
+    return (...args)=>{
+        let arg = args[0]
+        if(arg in cache){
+            console.log('Cachced');
+            return cache[arg]
+        }
+        else{
+            console.log('Counting');
+            let countResult = fn(arg)
+            cache[arg] = countResult;
+            return countResult
+        }
+    }
 }
+
+
+const objTest = {a:5}
+const objTest2 = {a:5}
+console.log(objTest === objTest2);
--- test/memoize.spec.js
+++ test/memoize.spec.js
@@ -1,16 +1,24 @@
 var assert = require('assert');
-const memoize = require('../src/memoize')
-
+const memoize = require('../src/memoize');
 
 describe('Memoize', function () {
-  describe('Check return 1', function () {
-    it('should return 1 from memoize', function () {
-      assert.equal(memoize(), 1);
-      assert.equal(memoize(), 1);
+
+  const addTen = (x)=>{
+    return x+10
+  }
+
+  const memTest = memoize(addTen);
+  
+  describe('Check return 4', function () {
+    it('should return 12', function () {
+      assert.equal(memTest(2), 12);
     });
-    it('should return 1 from memoize', function () {
-      assert.equal(memoize(), 1);
-      assert.equal(memoize(), 1);
+    it('should return 13 by count', function () {
+      assert.equal(memTest(3), 13);
     });
+    it('should return 13 from "cache" ', function () {
+      assert.equal(memTest(3), 13);
+    });
+    
   });
 });
