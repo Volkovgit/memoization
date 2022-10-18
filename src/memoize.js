315de19e2875c3b8efac323c07fb9f7d72190868
Aleksandr Volkov <guthub.volkov@gmail.com>
2022-10-18 19:45:30 +0300

    Мемоизация функции нескольких аргументов по value типу

--- src/memoize.js
+++ src/memoize.js
@@ -1,21 +1,13 @@
-module.exports = (fn)=>{
-    const cache = {}
-    return (...args)=>{
-        let arg = args[0]
-        if(arg in cache){
-            console.log('Cachced');
-            return cache[arg]
-        }
-        else{
-            console.log('Counting');
-            let countResult = fn(arg)
-            cache[arg] = countResult;
-            return countResult
-        }
+module.exports = (fn) => {
+  const cache = {};
+  return (...args) => {
+    let arg = args[0];
+    if (arg in cache) {
+      return cache[arg];
+    } else {
+      let countResult = fn(arg);
+      cache[arg] = countResult;
+      return countResult;
     }
-}
-
-
-const objTest = {a:5}
-const objTest2 = {a:5}
-console.log(objTest === objTest2);
+  };
+};
--- test/memoize.spec.js
+++ test/memoize.spec.js
@@ -1,24 +1,39 @@
 var assert = require('assert');
+const { expect } = require('chai');
 const memoize = require('../src/memoize');
 
 describe('Memoize', function () {
-
-  const addTen = (x)=>{
-    return x+10
-  }
-
-  const memTest = memoize(addTen);
-  
-  describe('Check return 4', function () {
-    it('should return 12', function () {
-      assert.equal(memTest(2), 12);
-    });
-    it('should return 13 by count', function () {
-      assert.equal(memTest(3), 13);
-    });
-    it('should return 13 from "cache" ', function () {
-      assert.equal(memTest(3), 13);
-    });
-    
+  it('Should be a function', () => {
+    expect(memoize).to.be.a('function');
   });
+
+  describe('Should return cached function that', () => {
+    const addTenTestFunction = (...args) => {
+      return args.reduce((sum,el) => sum+el);
+    };
+
+    const memTest = memoize(addTenTestFunction);
+
+    // describe('Check reterned values', function () {
+    //   it('should return 12', function () {
+    //     assert.equal(memTest(2), 12);
+    //   });
+    //   it('should return 13 by count', function () {
+    //     assert.equal(memTest(3), 13);
+    //   });
+    //   it('should return 13 from "cache" ', function () {
+    //     assert.equal(memTest(3), 13);
+    //   });
+    //   it('should return a10 from "cache" ', function () {
+    //     assert.equal(memTest('a'), 'a10');
+    //   });
+    // });
+
+    it('Is not equal to the original function',() => {
+      expect(memTest).to.be.a('function').and.not.to.equal(addTenTestFunction)
+    })
+
+  });
+
+  
 });
