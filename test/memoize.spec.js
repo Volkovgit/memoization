8f3e5d9bbb4ced6bb2510f61df0f287b539cbb4c
Aleksandr Volkov <guthub.volkov@gmail.com>
2022-10-20 20:10:33 +0300

    Исправляю тесты

--- test/memoize.spec.js
+++ test/memoize.spec.js
@@ -1,4 +1,3 @@
-var assert = require('assert');
 const { expect } = require('chai');
 const memoize = require('../src/memoize');
 const sinon = require('sinon');
@@ -20,54 +19,80 @@ describe('Memoize', function () {
     expect(functionResult).to.be.a(expectedResult);
   });
 
-  describe('Check return values', () => {
-    const addTen = (x) => {
+  describe('Cashed function', () => {
+    function addTen(x) {
       return x + 10;
-    };
+    }
 
-    const arraySumm = (args) => {
-      const arraySumm = args.reduce((summ, el) => summ + el);
-      return arraySumm
-    };
+    function summElements(...args) {
+      const result = args.reduce((sum, el) => sum + el);
+      return result;
+    }
 
-    let memoizedArraySumm;
+    function objectValues(obj){
+      return obj.values()
+    }
+
+    let memoizedSummElements;
     let memoizedAddTen;
     beforeEach(() => {
-      memoizedAddTen = memoize(addTen);
-      memoizedArraySumm = memoize(arraySumm);
+      addTenSpy = sinon.spy(addTen);
+      memoizedAddTen = memoize(addTenSpy);
+      summElementsSpy = sinon.spy(summElements);
+      memoizedSummElements = memoize(summElementsSpy);
     });
 
-    it('Return null if dont provided arguments in function', () => {
-      const functionResult = memoizedAddTen();
-      const expectedResult = null;
-      expect(functionResult).to.equal(expectedResult);
+    describe('Value-type data structure',() => {
+      
+    })
+
+    it('Is not equal to the original function', () => {
+      expect(memoize(addTen)).to.be.a('function').and.to.not.equal(addTen);
+      expect(memoize(summElements)).to.be.a('function').and.to.not.equal(summElements);
     });
 
-    it('Should return number above 10', () => {
-      const functionResult = memoizedAddTen(10);
-      const expectedResult = 20;
-      expect(functionResult).to.equal(expectedResult);
+    it('Returns correct values in case of the consequent calls with identical arguments', () => {
+      expect(memoizedAddTen(7)).to.equal(17); // #1
+      expect(memoizedAddTen(3)).to.equal(13); // #2
+      expect(memoizedAddTen(3)).to.equal(13); // #2
+      expect(memoizedAddTen(4)).to.equal(14); // #3
+      expect(memoizedAddTen(4)).to.equal(14); // #3
+      expect(memoizedAddTen(3)).to.equal(13); // #2
+      expect(memoizedAddTen(4)).to.equal(14); // #3
+      expect(memoizedAddTen(7)).to.equal(17); // #1
+      sinon.assert.callCount(addTenSpy, 3);
     });
 
-    it('Should return string with 10 in end', () => {
-      const functionResult = memoizedAddTen('one');
-      const expectedResult = 'one10';
-      expect(functionResult).to.equal(expectedResult);
+    it('Function called once', () => {
+      expect(memoizedAddTen(7)).to.equal(17); // #1
+      expect(memoizedAddTen(7)).to.equal(17); // #1
+      expect(memoizedAddTen(7)).to.equal(17); // #1
+      sinon.assert.calledOnce(addTenSpy);
     });
 
-    describe('Should return value from array', () => {
-      it('When array is all string', () => {
-        const functionResult = memoizedArraySumm(['one','two','three']);
-        const expectedResult = 'onetwothree';
-        console.log(functionResult);
-        expect(functionResult).to.equal(expectedResult);
-      });
-      it('When array is all numbers', () => {
-        const functionResult = memoizedArraySumm([1,2,3]);
-        const expectedResult = 'onetwothree';
-        console.log(functionResult);
-        expect(functionResult).to.equal(expectedResult);
-      });
+    it('Return correct value from function with many arguments', () => {
+      expect(memoizedSummElements(1, 2, 3, 4, 5)).to.equal(15);
+      sinon.assert.calledOnce(summElementsSpy);
     });
+
+    it('Function with identical arguments called once and return one value', () => {
+      expect(memoizedSummElements(1, 2, 3, 4, 5)).to.equal(15);
+      expect(memoizedSummElements(1, 2, 3, 4, 5)).to.equal(15);
+      expect(memoizedSummElements(1, 2, 3, 4, 5)).to.equal(15);
+      expect(memoizedSummElements(1, 2, 3, 4, 5)).to.equal(15);
+      sinon.assert.calledOnce(summElementsSpy);
+    });
+
+    it('Caches results of the consequent calls with identical arguments', () => {
+      expect(memoizedAddTen(1)).to.equal(11); // #1
+      sinon.assert.calledWith(addTenSpy, 1);
+      sinon.assert.calledOnce(addTenSpy);
+      addTenSpy.resetHistory();
+      expect(memoizedAddTen(1)).to.equal(11); // #2
+      sinon.assert.notCalled(addTenSpy);
+    });
+
+
+
   });
 });
